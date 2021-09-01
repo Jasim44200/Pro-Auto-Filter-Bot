3e9092905c1d5d15e4ba188d761fe6128a3c9cf1
jasim mhd <89708375+Jasim44200@users.noreply.github.com>
2021-09-01 09:17:43 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,50 +4,35 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hi {}!!</b>
 
-Iam Film zones group assistant manager""" 
+         START_TEXT = """<b>Hai {}!!</b>
 
-         HELP_TEXT = """
-
-Ask films from film zone groups Dont ask here"""
-
- <b>Bot Commands (Works Only In Groups) :</b>
-
-    -> <code>/add chat_id</code>
-                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
-     <code>/add @Username</code>
-
-     
-  -> <code>/del chat_id</code>
-                OR                  - To disconnect A Group With A Channel
-     <code>/del @Username</code>
-
-     
--> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
-
+<i>Iam assistant manager of film zone groups</i>"""    
     
--> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
+    HELP_TEXT = """<i><u>Usage Guide</u></i>
 
+<i>=> Add Bot To Any Channel As Admin With Add Members/ Invite Users Via Link
 
--> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
+=> Copy Channel ID
 
-            
--> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
+=> Use <code>/connect {channel id}</code> In Your Group To Connect With The Group
 
--> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
-            
-            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel """
+=> Use <code>/disconnect {channel id}</code> In Your Group To Disconnect From Your Group
 
+=> Use <code>/delall</code> In Your Group To Clear All Your Group  Connections (Owner Only)
 
-              HELP_TEXT = """
+Now You Are All Set And Ready To Go...
 
-Ask films from film zone groups Dont ask here """
+Just Send Any Text Will Try To Lookup In Channel And Provide You The Link</i>
+"""
+    
+    ABOUT_TEXT = """<b>➥ Name</b> : <code>Adv Auto Filter Bot</code>
 
-              ABOUT_TEXT = """
+<b>➥ Creator</b> : <b><i><a herf="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
 
-<b>➥ 📃Name</b> : <b>Pro Auto Filter Bot</b>
+<b>➥ Language</b> : <code>Python3</code>
 
-<b>➥ Creator</b> : <b>@jasimmhd</b> 
+<b>➥ Library</b> : <i><a herf="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0</a></i>
 
-<b>➥ Group id</b> : <b><a href="https://t.me/filmzone_123</a></b> """
+<b>➥ Source Code</b> : <i><a herf="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">GitHub</a></i>
+"""
