ddc24182fd083a8908cf1dfba277d6cd0b248c07
jasim mhd <89708375+Jasim44200@users.noreply.github.com>
2021-09-01 11:41:01 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,35 +4,54 @@
 
 class Translation(object):
     
+    START_TEXT = """<b>🙋‍♂️Hey {}!!</b>
 
-         START_TEXT = """<b>Hai {}!!</b>
+<b>Am Just A Pro Auto Filter Bot....😉</b>
 
-<i>Iam assistant manager of film zone groups</i>"""    
+</b>"""    
     
-    HELP_TEXT = """<i><u>Usage Guide</u></i>
+    HELP_TEXT = """
+<b>😉How To Use Me!😉?</b>
 
-<i>=> Add Bot To Any Channel As Admin With Add Members/ Invite Users Via Link
+<i>
+-> Add Me To Any Group And Make Me Admin
+-> Add Me To Your Desired Channel
+</i>
 
-=> Copy Channel ID
+<b>Bot Commands (Works Only In Groups) :</b>
 
-=> Use <code>/connect {channel id}</code> In Your Group To Connect With The Group
+    -> <code>/add chat_id</code>
+                OR                  - To Connect A Group With A Channel (Bot Should Be Admin With Full Previlages In Both Group And Channel)
+     <code>/add @Username</code>
+     
+    -> <code>/del chat_id</code>
+                OR                  - To disconnect A Group With A Channel
+     <code>/del @Username</code>
+     
+    -> <code>/delall</code>  - This Command Will Disconnect All Connected Channel With The Group And Deletes All Its File From DB
+    
+    -> <code>/settings</code> -  This Command Will Display You A Settings Pannel Instance Which Can Be Used To Tweek Bot's Settings Accordingly
 
-=> Use <code>/disconnect {channel id}</code> In Your Group To Disconnect From Your Group
+            -> <code>Channel</code> - Button Will Show You All The Connected Chats With The Group And Will Show Buttons Correspnding To There Order For Furthur Controls
+            
+            -> <code>Filter Types</code> - Button Will Show You The 3 Filter Option Available In Bot... Pressing Each Buttons Will Either Enable or Disable Them And This Will Take Into Action As Soon As You Use Them Without The Need Of A Restart
 
-=> Use <code>/delall</code> In Your Group To Clear All Your Group  Connections (Owner Only)
-
-Now You Are All Set And Ready To Go...
-
-Just Send Any Text Will Try To Lookup In Channel And Provide You The Link</i>
+            -> <code>Configure</code> - Button Will Helps You To Change No. of Pages/ Buttons Per Page/ Total Result Without Acutally Editing The Repo... Also It Provide Option To Enable/Disable For Showing Invite Link In Each Results
+            
+            -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
+            
+<b><a href="https://t.me/Mo_Tech_YT">MoTechYT</a></b>
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code>Adv Auto Filter Bot</code>
+    ABOUT_TEXT = """<b>➥ 📃Name</b> : <b>Pro Auto Filter Bot</b>
+    
+<b>➥ 😎Creator</b> : <b>@AlbertEinstein_TG</b> 
 
-<b>➥ Creator</b> : <b><i><a herf="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ 👨‍💻Editor</b> : <b>@Mrk_YT</b>
 
-<b>➥ Language</b> : <code>Python3</code>
+<b>➥ 🗣️Language</b> : <b>Python3</b>
 
-<b>➥ Library</b> : <i><a herf="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0</a></i>
+<b>➥ 📚Library</b> : <b><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></b>
 
-<b>➥ Source Code</b> : <i><a herf="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">GitHub</a></i>
+<b>➥ 📖Source Code</b> : <b><a href="https://github.com/MRK-YT/Pro-Auto-Filter-Bot">💥Click Me</a></b>
 """
